1b9eb8d4f9a10203187c818b635cc4dd72bef4b2
zhangxi <zhangxi@xingshulin.com>
2021-08-26 16:51:44 +0800

    [zhangxi] delete test code

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,17 +1,3 @@
-// export default (req, res) => {
-//     res.status(200).json({ text: 'Hello' })
-// }
-
-for (var index = 0; index < 10; index++) {
-    
-    
-    (function(){ //beginning of your current scope;
-        //...
-        var i = index
-        setTimeout(() => { 
-            
-            console.log(i)
-        }, 0)
-       })();
+export default (req, res) => {
+    res.status(200).json({ text: 'Hello' })
 }
-
